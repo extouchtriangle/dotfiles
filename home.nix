cf627b0f8c73d86079e45c325bdb664f2e6deb79
Easton Wei <eastonwei12@gmail.com>
2026-05-23 17:18:19 -0400

    debug

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
 
 {
   home.username = "easton";
-  home.nix = {
+  nix = {
     package = pkgs.nix;
     settings = {
       experimental-features = ["nix-command" "flakes"];
@@ -35,7 +35,7 @@
     CXX = "/bin/g++";
     MAMBA_EXE = "/usr/bin/micromamba";
     MAMBA_ROOT_PREFIX = "/home/easton/.local/share/mamba";
-    XDG_DATA_DIRS = "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
+    XDG_DATA_DIRS = "$XDG_DATA_DIRS:$HOME/.nix-profile/share";
   };
   home.sessionPath = [
     "/usr/condabin"
