f19f93914f39a9f2bf10a911795fa00c8004ca6e
Easton Wei <eastonwei12@gmail.com>
2026-05-23 17:14:42 -0400

    set xdg data dirs

--- home.nix
+++ home.nix
@@ -19,35 +19,9 @@
   ];
   targets.genericLinux.enable = true;
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".config/hypr".source = dotfiles/hypr;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/easton/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "emacs";
     VISUAL = "emacs";
@@ -55,6 +29,7 @@
     CXX = "/bin/g++";
     MAMBA_EXE = "/usr/bin/micromamba";
     MAMBA_ROOT_PREFIX = "/home/easton/.local/share/mamba";
+    XDG_DATA_DIRS = "$HOME/.nix-profile/share:$XDG_DATA_DIRS";
   };
   home.sessionPath = [
     "/usr/condabin"
