e461691d54871b22893de14541a3fcd2cbe80234
Easton Wei <eastonwei12@gmail.com>
2026-05-24 17:49:39 -0400

    try

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -224,4 +224,4 @@
               (read-only-mode 1))
             (pop-to-buffer out-buf)))))))
 (setq frame-title-format '("%b - emacs"))
-(setq sp-autoskip-opening-pair 'always-end)
+(setopt sp-autoskip-opening-pair 'always-end)
