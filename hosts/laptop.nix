525531b17b8a69c0732625d3ddc23021692f3119
Easton Wei <eastonwei12@gmail.com>
2026-05-24 10:15:54 -0400

    modularize (w all files)

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }: {
+{config, pkgs, ... }: {
+  imports = [
+    ../modules/arch-fish.nix
+  ];
   home.username = "easton";
   home.homeDirectory = "/home/easton";
 
   home.packages = [
   ];
+
 }
