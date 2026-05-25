7a608784a5397abfd848090b4f291c8853c13c76
Easton Wei <eastonwei12@gmail.com>
2026-05-25 10:23:03 -0400

    i think this should workc

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -227,6 +227,6 @@
 (after! smartparens
   (sp-with-modes '(tex-mode plain-tex-mode latex-mode LaTeX-mode text-mode)
     (sp-local-pair "$" nil :actions :rem)))
-(setq doom-user-dir "/home/easton/nix-dotfiles/dotfiles/doom/")
+(setq! doom-user-dir "/home/easton/nix-dotfiles/dotfiles/doom/")
 (after! org
-  (setq org-agenda-files '("~/org/todo.org")))
+  (setq! org-agenda-files '("~/org/todo.org")))
