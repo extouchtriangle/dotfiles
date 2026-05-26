21deb8936c33ae536b7a23ebbcce0a70196654dd
Easton Wei <eastonwei12@gmail.com>
2026-05-26 12:32:15 -0400

    add sioyek to laptop

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -3,6 +3,7 @@
     ../modules/arch-fish.nix
     ../modules/kitty.nix
     ../modules/doomemacs.nix
+    ../modules/sioyek.nix
   ];
   home.username = "easton";
   home.homeDirectory = "/home/easton";
